0f6e1aa534b2d40f36da41b4c37155697849bba3
elklemaitre <153718154+elklemaitre@users.noreply.github.com>
2024-12-12 11:28:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 const express = require('express');
 // Importer le module mongoose
 const mongoose = require('mongoose');
-// ================================================
-// Connexion à la base de données
-// ================================================
-// Quand je suis connecté à la bdd (evenementiel)
+
+
+
 mongoose.connection.once('open', () => {
     console.log("Connexion à la base de données effectué");
 });
@@ -14,27 +13,29 @@ mongoose.connection.once('open', () => {
 mongoose.connection.on('error', () => {
     console.log("Erreur dans la BDD");
 });
-
-// Se connecter sur mongodb (async)
-// Ca prend x temps à s'executer
 mongoose.connect("mongodb://localhost:27017/db_demo");
-// ================================================
-// Instancier un serveur et autoriser envokie json
-// ================================================
-// Instancier le server grace à express
+
+const article = mongoose.model('Article', { name: String }, 'articles');
+
 const app = express();
 
-// AUTORISER LE BACK A RECEVOIR DES DONNEES DANS LE BODY
 app.use(express.json());
-// ================================================
-// Les routes (url/point d'entrée)
-// ================================================
+
+app.get('/articles',async (request, response) => {
+
+    const product = await Product.find();
+
+    if (!product.length) return response.json({ message : "les differents articles" });
+
+    return response.json(product);
+
+})
+
 app.get('/todo', async (request, response) => {
     return response.json({ message : "Ceci est juste un exemple"}); 
 });
-// ================================================
-// Lancer le serveur
-// ================================================
+
+
 app.listen(3000, () => {
     console.log("Le serveur a démarré");
 });
